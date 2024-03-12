20c7fb61c2944d55ac80a6f92e0ff09263bd090c
Luis Acxis <luisacxis@gmail.com>
2024-03-12 03:07:36 -0600

    Ajustes para 2 medidores

--- Medidor_de_agua/Medidor_de_agua.ino
+++ Medidor_de_agua/Medidor_de_agua.ino
@@ -10,11 +10,13 @@ void println(T first, Types... other) {
 
 class WaterTank {
   private:
+    byte _length;
     byte _pins[7];
-    byte _length = 7;
 
   public:
-    WaterTank(byte pins[]) {
+    WaterTank(byte pins[], int length = 7) {
+      _length = length;
+      _pins[_length] = {};
       for (int i = 0; i < _length; i++) {
         _pins[i] = pins[i];
       }
@@ -55,6 +57,9 @@ class WaterTank {
       for (int i = _length - 1; i >= 0; i--) {
         const int pin = _pins[i];
         const bool isOn = digitalRead(pin);
+        if(pin == 24) {
+          println("Checking: ", isOn);
+        }
         if(isOn) {
           println("Verifing: ", pin);
           if(verifyStatusWhitSiblings(i)) {
@@ -68,17 +73,29 @@ class WaterTank {
     }
 };
 
-const byte bathroomTankInputs[] = {6, 7, 8, 9, 10, 11, 12};
-const WaterTank bathroomTank(bathroomTankInputs);
+const byte bathroomTankInputs1[] = {7, 8, 9, 10, 11, 12, 13};
+const WaterTank bathroomTank1(bathroomTankInputs1, 7);
+
+const byte bathroomTankInputs2[] = {14, 15, 16, 17, 18, 19, 6};
+const WaterTank bathroomTank2(bathroomTankInputs2, 6);
 
 void setup() {
   Serial.begin(9600);
-  bathroomTank.reset();
+  bathroomTank1.reset();
+  bathroomTank2.reset();
 }
 
 void loop() {
-  const int percentaje = bathroomTank.getFilled();
-  println(percentaje, "%");
   println("--------------");
-  delay(1000);
+  println("Tank 1");
+  const int percentaje1 = bathroomTank1.getFilled();
+  println(percentaje1, "%");
+  println("--------------");
+
+  println("Tank 2");
+  const int percentaje2 = bathroomTank2.getFilled();
+  println(percentaje2, "%");
+  println("--------------");
+
+  delay(3000);
 }
